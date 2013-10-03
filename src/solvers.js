1fd7a03228699502de3708970d374f7b3cc1ca19
Hack Reactor <hackreactor@HR-A1.local>
2013-10-02 22:24:51 -0700

    worked until this point on n-Queens (both nQeens tests failing... but are close)

--- src/solvers.js
+++ src/solvers.js
@@ -17,6 +17,7 @@ window.findNRooksSolution = function(n){
   if(n === 0){
     return solution;
   }
+
   var keyArray = _.range(0,n);
 
   var placeRook = function(keyArray, partialSolution){
@@ -49,7 +50,7 @@ window.countNRooksSolutions = function(n){
   if(n <= 1){
     return 1;
   }
-  
+
   var keysArray = [];
   var currentLevel = n;
 
@@ -76,17 +77,64 @@ window.countNRooksSolutions = function(n){
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+  var solution = [];
+  
+  var possibleSolutions = this.countNRooksSolutions(n);
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  // possibleSolutions.each(
+  //   if tests pass
+  //     great!
+  // )
+  
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  if(n <= 1){
+    return 1;
+  }
+  var keyArray = _.range(0, n);
+  var possibleSolutions = [];
+  var currentLevel = n;
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  var keyGenerator = function(partialSolution, currentLevel, keyArray){
+    if(currentLevel === 0){
+      possibleSolutions.push(partialSolution);
+      return;
+    }
+    for(var i = (n-currentLevel); i < n; i++){
+      // // building the entire matrix e.g. [0, 1, 2]
+      // var subResults = [];
+      // subResults.push(i);
+      // keyGenerator(partialSolution.concat(subResults), currentLevel - 1);
+      var row = [];
+      // build row
+      for(var j = 0; j < n; j++){
+        if(j === keyArray[i]){
+          row.push(1);
+        }else{
+          row.push(0);
+        }
+      }
+      var tempSolution =  [];
+      tempSolution.push(row);
+      keyGenerator(partialSolution.concat(tempSolution), currentLevel - 1, keyArray);
+    }
+  };
+  keyGenerator([], n, keyArray);
+  
+  var queenMatrix = _.filter(possibleSolutions, function(matrix){
+    var tempBoard = new Board(matrix);
+    if(!tempBoard.hasAnyMajorDiagonalConflicts() && !tempBoard.hasAnyMinorDiagonalConflicts()){
+      console.log('check matrix', matrix);
+      return matrix;
+    }
+  });
+
+  console.log('Number of solutions for ' + n + ' queens:', queenMatrix.length);
+  return queenMatrix.length;
 };
