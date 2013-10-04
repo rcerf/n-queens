a30a2c3be858b0bb9b091d6b9a46aad5ae7a01c7
Hack Reactor <hackreactor@HR-A1.local>
2013-10-03 18:50:05 -0700

    Worked to here before 6:30 lecture

--- src/solvers.js
+++ src/solvers.js
@@ -90,7 +90,6 @@ window.countNQueensSolutions = function(n){
   if(n <= 1){
     return 1;
   }
-  n = 4;
 
   var solutionsArray = [];
   var masterKey = _.range(0, n);
@@ -101,7 +100,6 @@ window.countNQueensSolutions = function(n){
       console.log(partialSolution);
       return;
     }
-    
     // call queenKey here
     var queenKey = queenKeyGenerator(masterKey, partialSolution, currentLevel);
 
@@ -111,11 +109,12 @@ window.countNQueensSolutions = function(n){
       //i === location of queen
       subResults.push(queenKey[i]);
       // trim for queens
-      var returnKey = _.filter(masterKey, function(value){
-        if(value !== queenKey[i]){
-          return value;
+      var returnKey = [];
+      for(var i = 0; i < masterKey.length; i++){
+        if(masterKey[i] !== queenKey[i]){
+          returnKey.push(masterKey[i]);
         }
-      });
+      }
       keyGenerator(partialSolution.concat(subResults), currentLevel - 1, returnKey);
     }
   };
@@ -123,77 +122,22 @@ window.countNQueensSolutions = function(n){
   var queenKeyGenerator = function(masterKey, partialSolution, currentLevel){
     var difference = n - currentLevel;
     var queenKey =  masterKey.slice(0);
+    
     for(var j = 0; j < partialSolution.length; j++){
       var minorDiag = partialSolution[j] - difference;
       var majorDiag = partialSolution[j] + difference;
       difference--;
-      queenKey = _.filter(queenKey, function(value) {
-        if(value !== minorDiag && value !== majorDiag){
-          return value;
+      
+      for(var k = 0; k < queenKey.length; k++){
+        if(queenKey[k] === minorDiag || queenKey[k] === majorDiag){
+          queenKey.splice(k,1);
         }
-      });
+      }
     }
     return queenKey;
-  }
-  debugger;
+  };
+  //debugger;
   keyGenerator([], n, masterKey);
   console.log('Number of solutions for ' + n + ' queens:', solutionsArray.length);
   return solutionsArray.length;
-
-  // var keyTrim = function(keyArray, i, currentLevel){
-  //   var difference = n - currentLevel;
-  //   if(currentLevel === 0){
-  //     return keyArray.slice(1);
-  //   }
-  //   var subKey = keyArray.slice(0);
-  //   subKey[n - currentLevel - (n - subKey.length)] = _.filter(subKey[n-currentLevel - (n - subKey.length)], function(value){
-  //     if(value !== i-difference && value !== i && value !== i + difference){
-  //       return value;
-  //     }
-  //   });
-  //   return keyTrim(subKey, i, currentLevel -1);
-  // }
-
-  
-
-  // if(n <= 1){
-  //   return 1;
-  // }
-  // var keyArray = _.range(0, n);
-  // var possibleSolutions = [];
-  // var currentLevel = n;
-
-  // var keyGenerator = function(partialSolution, currentLevel, keyArray){
-  //   if(currentLevel === 0){
-  //     possibleSolutions.push(partialSolution);
-  //     return;
-  //   }
-  //   for(var i = (n-currentLevel); i < n; i++){
-  //     // // building the entire matrix e.g. [0, 1, 2]
-  //     var row = [];
-  //     // build row
-  //     for(var j = 0; j < n; j++){
-  //       if(j === keyArray[i]){
-  //         row.push(1);
-  //       }else{
-  //         row.push(0);
-  //       }
-  //     }
-  //     var tempSolution =  [];
-  //     tempSolution.push(row);
-  //     keyGenerator(partialSolution.concat(tempSolution), currentLevel - 1, keyArray);
-  //   }
-  // };
-  // keyGenerator([], n, keyArray);
-  
-  // var queenMatrix = _.filter(possibleSolutions, function(matrix){
-  //   var tempBoard = new Board(matrix);
-  //   if(!tempBoard.hasAnyMajorDiagonalConflicts() && !tempBoard.hasAnyMinorDiagonalConflicts()){
-  //     console.log('check matrix', matrix);
-  //     return matrix;
-  //   }
-  // });
-
-  // console.log('Number of solutions for ' + n + ' queens:', queenMatrix.length);
-  // return queenMatrix.length;
 };
